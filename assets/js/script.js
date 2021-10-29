7b5a62ceb8ab28241883225aa5c2a6b6d08b9e23
Erinn Schneider <erinnaschneider@gmail.com>
2021-10-29 14:06:42 -0400

    setting up password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 // Assignment code here
 
+function howLong() {
+  var passwordLength = window.prompt("How long do you want your password to be?");
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null || isNaN(passwordLength)) {
+    window.alert("You need to pick a number between 8 and 128!");
+  }
+  return passwordLength;
+};
+
+function passwordRequirements () {
+  var lowerCase = window.confirm("Would you like to use lowercase letters?");
+  if (lowerCase) {
+    
+  }
+}
+
+function generatePassword () {
+  var passwordLength = howLong();
+  console.log(passwordLength);
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,7 +30,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
